25e0ccc0ff1812aae13b0a89c337e10f877a63d7
Aleksandr-Abramov <ale19850505@gmail.com>
2023-09-03 15:12:47 +0300

    features: 2.13. Шаблонизируй это. 0.2

--- src/constanst.js
+++ src/constanst.js
@@ -1,13 +1,13 @@
 export const POINT_TYPE = [
-  "Taxi",
-  "Bus",
-  "Train",
-  "Ship",
-  "Drive",
-  "Flight",
-  "Check-in",
-  "Sightseeing",
-  "Restaurant",
+  'Taxi',
+  'Bus',
+  'Train',
+  'Ship',
+  'Drive',
+  'Flight',
+  'Check-in',
+  'Sightseeing',
+  'Restaurant',
 ];
 
 export const POINT_TYPE_ICON = new Map();
@@ -16,26 +16,26 @@ POINT_TYPE.forEach((item) =>
 );
 
 export const OOFFER_TITLE = [
-  "Add luggage",
-  "Switch to comfort class",
-  "Add meal",
-  "Choose seats",
-  "Travel by train",
+  'Add luggage',
+  'Switch to comfort class',
+  'Add meal',
+  'Choose seats',
+  'Travel by train',
 ];
-export const DESTINATIONS = ["Chamonix", "Amsterdam", "Geneva"];
+export const DESTINATIONS = ['Chamonix', 'Amsterdam', 'Geneva'];
 
 export const DESCRIPTION = [
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-  "Cras aliquet varius magna, non porta ligula feugiat eget",
-  "Fusce tristique felis at fermentum pharetra",
-  "Aliquam id orci ut lectus varius viverra",
-  "Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante",
-  "Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum",
-  "Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui",
-  "Sed sed nisi sed augue convallis suscipit in sed felis",
-  "Aliquam erat volutpat",
-  "Nunc fermentum tortor ac porta dapibus",
-  "In rutrum ac purus sit amet tempus.",
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+  'Cras aliquet varius magna, non porta ligula feugiat eget',
+  'Fusce tristique felis at fermentum pharetra',
+  'Aliquam id orci ut lectus varius viverra',
+  'Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante',
+  'Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum',
+  'Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui',
+  'Sed sed nisi sed augue convallis suscipit in sed felis',
+  'Aliquam erat volutpat',
+  'Nunc fermentum tortor ac porta dapibus',
+  'In rutrum ac purus sit amet tempus.',
 ];
 
 export const Price = {
--- src/main.js
+++ src/main.js
@@ -11,23 +11,3 @@ const pointModel = new PointModel(offerIds, destinationModel.getDestinations());
 
 const boardPresenter = new BoardPresenter({pointModel, offerModel, destinationModel});
 boardPresenter.init();
-
-
-// class a {
-//     getA() {
-//         console.log(3213);
-//     }
-// }
-// class c {
-//     constructor(service) {
-//     this.service = service;
-//     this.points = this.service.getA();
-//     }
-
-//     getCCC() {
-//         return this.points;
-//     }
-// }
-// const b = new a();
-// const dsad = new c(b);
-// dsad.getCCC();
